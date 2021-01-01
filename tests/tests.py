eec3b97820c10278729087a911b9628ab8ab85ec
Rizwan Hasan <rizwan.hasan486@gmail.com>
2021-01-02 02:52:02 +0600

    updated

--- tests/tests.py
+++ tests/tests.py
@@ -17,7 +17,7 @@ outputFileXLSX: str = "test.xlsx"
 inputFileSQLITE3: str = "datasets/test.sqlite3"
 outputFileSQLITE3: str = "test.sqlite3"
 
-
+# Preparation ↓
 def test_prepare():
     try:
         shutil.rmtree("rubaialter")
@@ -26,6 +26,7 @@ def test_prepare():
     shutil.copytree("../rubaialter", "rubaialter")
 
 
+# CSV to CSV ↓
 def test_csv_to_csv():
     global inputFileCSV, outputFileCSV
     shutil.copyfile(inputFileCSV, outputFileCSV)
@@ -37,12 +38,30 @@ def test_csv_to_csv():
     assert output == "The file is already in csv format."
 
 
+# CSV to XLS ↓
 def test_csv_to_xls():
-    global inputFileCSV, outputFileCSV
+    global inputFileCSV, outputFileXLS
     subprocess.check_call(["python", "-m", "rubaialter", inputFileCSV, "--xls"])
     assert os.path.exists(outputFileXLS)
     os.remove(outputFileXLS)
 
 
+# CSV to XLSX ↓
+def test_csv_to_xlsx():
+    global inputFileCSV, outputFileXLSX
+    subprocess.check_call(["python", "-m", "rubaialter", inputFileCSV, "--xlsx"])
+    assert os.path.exists(outputFileXLSX)
+    os.remove(outputFileXLSX)
+
+
+# CSV to SQLITE3 ↓
+def test_csv_to_sqlite3():
+    global inputFileCSV, outputFileSQLITE3
+    subprocess.check_call(["python", "-m", "rubaialter", inputFileCSV, "--sqlite3"])
+    assert os.path.exists(outputFileSQLITE3)
+    os.remove(outputFileSQLITE3)
+
+
+# Cleanup ↓
 def test_clean():
     shutil.rmtree("rubaialter")
